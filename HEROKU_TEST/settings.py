0244f3b0d693a3b6bebf45d2348e37a3000b3763
ysgo <wjion8428@gmail.com>
2019-11-25 01:50:34 +0900

    heroku deploy error

--- HEROKU_TEST/settings.py
+++ HEROKU_TEST/settings.py
@@ -1,6 +1,6 @@
 import os
 from decouple import config
-
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -107,3 +107,5 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
