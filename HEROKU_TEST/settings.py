b7f13f60835eaf5506a611f4e1b5b335520c569a
ysgo <wjion8428@gmail.com>
2019-11-25 01:46:59 +0900

    heroku deploy error

--- HEROKU_TEST/settings.py
+++ HEROKU_TEST/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['young-wave-47275.herokuapp.com']
 
 
 # Application definition
